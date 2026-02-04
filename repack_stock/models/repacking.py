191f62dcf3926cb426c11d1796e74c26913d4913
Yayan H <yayannn.hd@gmail.com>
2026-02-04 15:31:31 +0700

    repack update

--- repack_stock/models/repacking.py
+++ repack_stock/models/repacking.py
@@ -25,10 +25,34 @@ class StockRepackLine(models.Model):
     product_b_id = fields.Many2one(
         "product.product",
         string="Product Repack",
-        required=True
     )
 
     qty_b = fields.Float(
         string="Qty",
+    )
+
+    repack_output_ids = fields.One2many(
+        "stock.repack.output",
+        "repack_line_id",
+        string="Hasil Repack"
+    )
+
+class StockRepackOutput(models.Model):
+    _name = "stock.repack.output"
+    _description = "Stock Repack Output"
+
+    repack_line_id = fields.Many2one(
+        "stock.repack.line",
+        ondelete="cascade"
+    )
+
+    product_b_id = fields.Many2one(
+        "product.product",
+        string="Product Hasil",
+        required=True
+    )
+
+    qty_b = fields.Float(
+        string="Qty Hasil",
         required=True
     )
--- repack_stock/models/stock_picking.py
+++ repack_stock/models/stock_picking.py
@@ -12,22 +12,60 @@ class StockPicking(models.Model):
 
     is_repack_done = fields.Boolean(default=False)
 
+    def button_validate(self):
+
+        for picking in self:
+
+            if picking.repack_line_ids:
+                StockMoveLine = self.env['stock.move.line']
+
+                moves_b = []
+                for line in picking.repack_line_ids:
+                    StockMoveLine.create({
+                        'product_id': line.product_a_id.id,
+                        'product_uom_id': line.product_a_id.uom_id.id,
+                        'quantity': line.qty_a,
+                        'location_id': picking.location_id.id,      
+                        'location_dest_id': picking.location_dest_id.id,
+                        'company_id': picking.company_id.id,
+                    })
+
+                    StockMoveLine.create({
+                        'product_id': line.product_a_id.id,
+                        'product_uom_id': line.product_a_id.uom_id.id,
+                        'quantity': line.qty_a,
+                        'location_id': picking.location_dest_id.id,
+                        'location_dest_id': picking.location_id.id,
+                        'company_id': picking.company_id.id,
+                    })
+                    
+                    for l in line.repack_output_ids:
+                        moves_b.append((0, 0, {
+                            'picking_id': picking.id,
+                            'name': l.product_b_id.name,
+                            'product_id': l.product_b_id.id,
+                            'product_uom_qty': l.qty_b,
+                            'quantity': l.qty_b,
+                            'product_uom': l.product_b_id.uom_id.id,
+                            'location_id': picking.location_id.id,
+                            'location_dest_id': picking.location_dest_id.id,
+                            'company_id': picking.company_id.id,
+                        }))
+                picking.move_ids_without_package = moves_b
+
+                picking.is_repack_done = True
+
+        return super().button_validate()
+
     def action_repack(self):
         StockMoveLine = self.env['stock.move.line']
 
         for picking in self:
 
-            if picking.is_repack_done:
-                raise UserError("Repack sudah diproses.")
-
-            if not picking.repack_line_ids:
-                raise UserError("Isi data repack dulu.")
-
             moves_b = []
-
             for line in picking.repack_line_ids:
                 StockMoveLine.create({
-                    'picking_id': picking.id,
+                    # 'picking_id': picking.id,
                     'product_id': line.product_a_id.id,
                     'product_uom_id': line.product_a_id.uom_id.id,
                     'quantity': line.qty_a,
@@ -36,22 +74,25 @@ class StockPicking(models.Model):
                 })
 
                 StockMoveLine.create({
-                    'picking_id': picking.id,
+                    # 'picking_id': picking.id,
                     'product_id': line.product_a_id.id,
                     'product_uom_id': line.product_a_id.uom_id.id,
                     'quantity': line.qty_a,
                     'location_id': picking.location_dest_id.id,
                     'location_dest_id': picking.location_id.id 
                 })
-
-                moves_b.append((0, 0, {
-                    'name': line.product_b_id.name,
-                    'product_id': line.product_b_id.id,
-                    'product_uom_qty': line.qty_b,
-                    'product_uom': line.product_b_id.uom_id.id,
-                    'location_id': picking.location_id.id,
-                    'location_dest_id': picking.location_dest_id.id,
-                }))
+                
+                for l in line.repack_output_ids:
+                    moves_b.append((0, 0, {
+                        'picking_id': picking.id,
+                        'name': l.product_b_id.name,
+                        'product_id': l.product_b_id.id,
+                        'product_uom_qty': l.qty_b,
+                        'quantity': l.qty_b,
+                        'product_uom': l.product_b_id.uom_id.id,
+                        'location_id': picking.location_id.id,
+                        'location_dest_id': picking.location_dest_id.id,
+                    }))
             picking.move_ids_without_package = moves_b
 
             picking.is_repack_done = True
